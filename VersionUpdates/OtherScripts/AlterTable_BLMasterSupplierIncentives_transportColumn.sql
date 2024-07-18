f34f9c697520e442ee58b6d5d42ed70ad7bfa44e
ShehanAmarasooriya <d.shehan.amarasooriya@gmail.com>
2024-07-18 04:32:57 +0000

    deleted

--- VersionUpdates/OtherScripts/AlterTable_BLMasterSupplierIncentives_transportColumn.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE BLMasterSupplierIncentives
-ALTER COLUMN transportIncentive DECIMAL(18,2)  NOT NULL  
-
-ALTER TABLE BLMasterSupplierIncentives ADD CONSTRAINT DF_transportIncentive DEFAULT (0) FOR transportIncentive
